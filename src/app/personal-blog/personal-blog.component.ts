ca9b4c884276954bc0ffa2b24e96d66107f4d393
Anna-Marie Cooper <annamarie@rogueminx.com>
2018-03-30 12:30:20 -0700

    blog posts show in personal blog component.

--- src/app/personal-blog/personal-blog.component.ts
+++ src/app/personal-blog/personal-blog.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Blog } from '../models/blog.model';
+import { Router } from '@angular/router'
+import { BlogService } from '../blog.service';
+import { FirebaseListObservable } from 'angularfire2/database';
+
 
 @Component({
   selector: 'app-personal-blog',
   templateUrl: './personal-blog.component.html',
-  styleUrls: ['./personal-blog.component.css']
+  styleUrls: ['./personal-blog.component.css'],
+  providers: [BlogService]
 })
 export class PersonalBlogComponent implements OnInit {
+  blogs: FirebaseListObservable<any[]>;
 
-  constructor() { }
+  constructor(private router: Router, private BlogService: BlogService) { }
 
   ngOnInit() {
+    this.blogs = this.BlogService.getBlogPosts();
   }
 
 }
