47c88f380cec505a755de19de782e70b8d149689
Anna-Marie Cooper <annamarie@rogueminx.com>
2018-03-30 14:39:08 -0700

    add new project model and service.

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,17 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { BlogService } from '../blog.service';
 import { Blog } from '../models/blog.model';
+import { Project } from '../models/project.model';
+import { ProjectService } from '../project.service';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.css'],
-  providers: [BlogService]
+  providers: [BlogService, ProjectService]
 })
 
 export class AdminComponent implements OnInit {
 
-  constructor(private BlogService: BlogService) { }
+  constructor(private BlogService: BlogService, private ProjectService: ProjectService) { }
 
   ngOnInit() {
   }
@@ -19,6 +21,11 @@ export class AdminComponent implements OnInit {
   submitBlog(title: string, postBody: string){
     let newBlogPost: Blog = new Blog(title, postBody);
     this.BlogService.addBlogPost(newBlogPost);
+  }
+
+  submitProject(title: string, description: string, challenges: string, projectURL: string){
+    let newProject: Project = new Project(title, description, challenges, projectURL);
+    this.ProjectService.addNewProject(newProject);
   }
 
 }
---
+++ src/app/project.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Project } from './models/project.model';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+
+@Injectable()
+export class ProjectService {
+  projects: FirebaseListObservable<any[]>;
+
+  constructor(private database: AngularFireDatabase) {
+    this.projects = database.list('projects');
+  }
+
+  getProjects(){
+    return this.projects;
+  }
+
+  addNewProject(newProject: Project){
+    this.projects.push(newProject);
+  }
+
+}
