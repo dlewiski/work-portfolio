32ba763ca27bf9dfc522faf22f3aab90a11ead6a
Anna-Marie Cooper <annamarie@rogueminx.com>
2018-03-30 08:54:51 -0700

    add starter routing to routing.ts

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,6 +1,27 @@
 import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { PersonalBlogComponent } from './personal-blog/personal-blog.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
-const appRoutes: Routes = [ ];
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'about-me',
+    component: AboutMeComponent
+  },
+  {
+    path: 'blog',
+    component: PersonalBlogComponent
+  },
+  {
+    path: 'portfolio',
+    component: PortfolioComponent
+  }
+];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
