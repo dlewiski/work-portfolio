663532d3be1900ba1830af1c343c08328d86a726
Anna-Marie Cooper <annamarie@rogueminx.com>
2018-03-30 11:00:29 -0700

    research file uploading, add admin link to footer, create blog model.

--- src/app/blog.service.ts
+++ src/app/blog.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@angular/core';
+import { Blog } from './models/blog.model';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 
 @Injectable()
 export class BlogService {
+  blogs: FirebaseListObservable<any[]>;
 
-  constructor() { }
+  constructor(private database: AngularFireDatabase) {
+    this.blogs = database.list('blogs');
+  }
+
+  getBlogPosts(){
+     return this.blogs;
+   }
 
 }
---
+++ src/app/models/blog.model.ts
@@ -0,0 +1,4 @@
+export class Blog {
+  public author: string = "Anna Marie Cooper";
+  constructor(public title: string, public postBody: string, public date: string) { }
+}
