7d4571bf94f01c55ad6b9b12e8e05f325a02e22c
Anna-Marie Cooper <annamarie@rogueminx.com>
2018-04-05 09:43:28 -0700

    successfully populate a list of projects in the portfolio section.

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Project } from '../models/project.model';
+import { Router } from '@angular/router'
+import { ProjectService } from '../services/project.service';
+import { FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
-  styleUrls: ['./portfolio.component.css']
+  styleUrls: ['./portfolio.component.css'],
+  providers: [ ProjectService ]
 })
 export class PortfolioComponent implements OnInit {
-
-  constructor() { }
+  projects: FirebaseListObservable<any[]>;
+  constructor(private router: Router, private ProjectService: ProjectService) { }
 
   ngOnInit() {
+    this.projects = this.ProjectService.getProjects();
+  }
+
+  goToProjectDetailPage(clickedProject) {
+    this.router.navigate(['projects', clickedProject.$key])
   }
 
 }
